6b60585fa502b47161a7a6a7d471402ba5275af8
GagoilKim <cplkimdh@gmail.com>
2023-01-02 16:37:43 +0900

    feat: add expenseFilter and apply child to parent data transfer

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,20 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
 import Card from '../UI/Card';
+import ExpensesFilter from "./ExpensesFilter";
+import React, {useState} from 'react';
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = selectedYear => {
+    // console.log(selectedYear);
+    setFilteredYear(selectedYear);
+  };
+
   return (
+    <div>
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
     <Card className="expenses">
       {props.items.map(item => (
          <ExpenseItem
@@ -13,6 +24,8 @@ const Expenses = (props) => {
        />
       ))}
     </Card>
+    </div>
+
   );
 }
 
